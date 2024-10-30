d9a6051ab7a393c0d650ddf321d949c62bbf84ca
Steven <stevengf14@gmail.com>
2024-10-29 23:18:58 -0500

    leer fichero de entrada

---
+++ src/main/java/ec/com/linkedinlearning/domina/java/c_01_03/Dato.java
@@ -0,0 +1,51 @@
+package ec.com.linkedinlearning.domina.java.c_01_03;
+
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author steve
+ */
+public class Dato {
+
+    private static final NumberFormat NF
+            = NumberFormat.getCurrencyInstance();
+    private static final DateTimeFormatter DTF
+            = DateTimeFormatter.ofPattern("EEEE, d 'de' MMMM 'de' yyyy");
+
+    private String nombre;
+    private LocalDate fecha;
+    private int sueldo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(int sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " / " + DTF.format(fecha) + " / " + sueldo;
+    }
+
+}
